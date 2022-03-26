f370c94b459e20fbe3cf209d3842ccb6af398593
dangtrannam <dangtrannam2001@gmail.com>
2022-03-26 18:11:16 +0700

    ViewImageScreen

--- App.tsx
+++ App.tsx
@@ -17,9 +17,10 @@ import {
   useDeviceOrientation,
 } from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
