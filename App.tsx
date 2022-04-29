62ab1701242849a9d3b54ed5a239d2a6b0660cd6
dangtrannam <dangtrannam2001@gmail.com>
2022-04-29 23:49:10 +0700

    encapsulating styles

--- App.tsx
+++ App.tsx
@@ -16,8 +16,10 @@ import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
@@ -28,22 +30,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "600",
-          fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-          fontStyle: "italic",
-          color: "tomato",
-          textTransform: "capitalize",
-          textDecorationLine: "underline",
-          textAlign: "justify",
-          lineHeight: 40,
-        }}
-      >
-        I love React Native! This is my first React Native App! Here's some more
-        text
-      </Text>
+      <AppText>I love React !!!!</AppText>
     </View>
   );
 }
