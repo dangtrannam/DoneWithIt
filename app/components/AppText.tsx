9bc9ce6895a40aa6d1d59825877d83dbc7988717
dangtrannam <dangtrannam2001@gmail.com>
2022-04-30 00:06:31 +0700

    organizing styles

--- app/components/AppText.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
-
-function AppText({ children }) {
-  return <Text>{children}</Text>;
-}
-
-export default AppText;
