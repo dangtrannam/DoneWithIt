37e248e58a745447811200ab1c6523495652fbbb
dangtrannam <dangtrannam2001@gmail.com>
2022-04-30 00:47:47 +0700

    listing details screen

---
+++ app/screens/ListingDetailsScreen.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
+import colors from "../config/colors";
+
+function ListingDetailsScreen() {
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale!</AppText>
+        <AppText style={styles.subTitle}>$100</AppText>
+        <View style={styles.usersContainer}>
+          <ListItem
+            image={require("../assets/jacket.jpg")}
+            title="Mosh Hamedani"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  usersContainer: {
+    marginVertical: 40,
+  },
+});
+
+export default ListingDetailsScreen;
