74a770af09b78ec18a99f7a4b7a38dd7b01aee46
dangtrannam <dangtrannam2001@gmail.com>
2022-05-01 05:56:07 +0700

    switch

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import { Switch } from "react-native";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 export default function App() {
+  const [isNew, setIsNew] = useState<boolean>(false);
   return (
     <Screen>
       <AppTextInput icon={"email"} placeholder="email" />
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
