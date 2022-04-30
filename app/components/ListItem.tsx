5a0d638f60e8502ab8426d1e3eeb8b7e935d65ed
dangtrannam <dangtrannam2001@gmail.com>
2022-05-01 04:58:42 +0700

    Messages Screen

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,34 +1,69 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
-import colors from "../config/colors";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
 import AppText from "./AppText";
+import {
+  Swipeable,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
-function ListItem({ title, subTitle, image }) {
+import colors from "../config/colors";
+
+function ListItem({
+  title,
+  subTitle,
+  image,
+  IconComponent = null,
+  onPress,
+  renderRightActions,
+}) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    // android needs to wrap the gesture handler in a gesture handler root view
+    <GestureHandlerRootView>
+      <Swipeable renderRightActions={renderRightActions}>
+        <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+          <View style={styles.container}>
+            {IconComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
+              <AppText style={styles.title}>{title}</AppText>
+              {subTitle && (
+                <AppText style={styles.subTitle}>{subTitle}</AppText>
+              )}
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
-  },
-  title: {
-    fontWeight: "500",
   },
   subTitle: {
     color: colors.medium,
   },
+  title: {
+    fontWeight: "500",
+  },
 });
+
 export default ListItem;
