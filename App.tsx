a5f135e6aaf0021968284443f86caacbb446d35d
dangtrannam <dangtrannam2001@gmail.com>
2022-05-01 06:04:07 +0700

    custom picker

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import { Switch } from "react-native";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
+
 export default function App() {
-  const [isNew, setIsNew] = useState<boolean>(false);
   return (
     <Screen>
+      <AppPicker icon={"apps"} placeholder={"Category"} />
       <AppTextInput icon={"email"} placeholder="email" />
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
