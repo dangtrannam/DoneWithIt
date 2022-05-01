1f401c375b01a4107a6ef906cc5feb587d1d4907
dangtrannam <dangtrannam2001@gmail.com>
2022-05-01 07:32:41 +0700

    formik & yup

--- App.tsx
+++ App.tsx
@@ -1,26 +1,6 @@
-import AppTextInput from "./app/components/AppTextInput";
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
 import { useState } from "react";
-
-const categories = [
-  { label: "Fruits", value: 1 },
-  { label: "Clothes", value: 2 },
-  { label: "Electronics", value: 3 },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(null);
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={setCategory}
-        items={categories}
-        icon={"apps"}
-        placeholder={"Category"}
-      />
-      <AppTextInput icon={"email"} placeholder="email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
---
+++ app/screens/LoginScreen.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import Screen from "../components/Screen";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function LoginScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              placeholder={"Email"}
+              icon="email"
+              onChangeText={handleChange("email")}
+            />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry
+              textContentType="password"
+              placeholder={"Password"}
+              icon="lock"
+              onChangeText={handleChange("password")}
+            />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+            <AppButton title={"Login"} onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+export default LoginScreen;
